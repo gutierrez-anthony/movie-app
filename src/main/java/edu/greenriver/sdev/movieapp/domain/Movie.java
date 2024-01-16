e22621340e2fb99e93e1ce3936e991fc6bc35d4d
gutierrez-anthony <gutierrez.anthony@student.greenriver.edu>
2024-01-16 14:33:18 -0800

    Configured Movie.Java class to act as a Database entity and added JPA and H2 configurations.

--- src/main/java/edu/greenriver/sdev/movieapp/domain/Movie.java
+++ src/main/java/edu/greenriver/sdev/movieapp/domain/Movie.java
@@ -1,19 +1,20 @@
 package edu.greenriver.sdev.movieapp.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 // @Data adds getters/setters/toString()/equals/hashcode...
+@Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Movie {
-
-
+    @Id
     private String title;
     private int year;
-
     private String genre;
     private String rating;
     private boolean international;
