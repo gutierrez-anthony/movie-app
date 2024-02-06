6413dc54093e9fb43968392eca7a3cb340805fc7
gutierrez-anthony <gutierrez.anthony@student.greenriver.edu>
2024-02-06 14:02:58 -0800

    Added the post method

--- src/main/resources/static/scripts/data.js
+++ src/main/resources/static/scripts/data.js
@@ -8,6 +8,11 @@ window.onload = async function () {
     const data = await response.json();
     showMovies(data);
 
+    /*
+        When converting a json text to a javascript object -> json.parse()
+        When converting a js object to a json text -> json.stringify()
+    */
+
     /* Promises
     fetch(uri, config)
         .then(function (response) {
@@ -21,21 +26,51 @@ window.onload = async function () {
             showMovies(data);
         });*/
 
+    // Select form button and handle form submission
+    const button = document.querySelector("button");
+    button.onclick = addMovie;
     // update our UI
+}
+
+async function addMovie(event) {
+    // Stops form from submitting
+    event.preventDefault();
+
+    const newMovie = {
+        title: document.querySelector("#title").value,
+        genre: document.querySelector("genre").value
+    };
+    //console.log("Button Clicked");
+    const uri = "http://localhost:3000/api/v1/movies";
+    const config = {
+        method: "post",
+        headers: {"Content-Type": "application/json"
+        },
+        body: JSON.stringify(newMovie)
+    }
+
+    const response = await fetch(uri, config);
+    const data = await response.json();
+
+    const section = document.querySelector("#movies");
+    addMovieSection(section, data);
 }
 
 function showMovies(movies) {
     const section = document.querySelector("#movies");
-    console.log(section);
+    //console.log(section);
 
     // Loop over each of my movies and create a nested elements
     for (let i = 0; i < movies.length; i++) {
         const movie = movies[i];
-        section.innerHTML += `<div class="movie">
+        addMovieSection(section, movie);
+    }
+}
+
+function addMovieSection(section, movie) {
+    section.innerHTML += `<div class="movie">
                                     <h2>${movie.title}</h2>
                                     <p>ID #${movie.id}</p>
                                     <p>Genre: ${movie.genre}</p>
                                 </div>`;
-    }
 }
-
